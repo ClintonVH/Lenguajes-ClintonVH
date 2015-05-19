706fdf5e3fb0d7fedccfb564d08f182fad924cdf
Clinton Vazquez Hermosillo <clinton_wero@hotmail.com>
2015-05-18 19:45:44 -0500

    Update LenguajesCVH.java

--- Lenguajes-CVH/src/lenguajes/cvh/LenguajesCVH.java
+++ Lenguajes-CVH/src/lenguajes/cvh/LenguajesCVH.java
@@ -15,7 +15,7 @@ public class LenguajesCVH {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Mi Primer Proyecto");
+        System.out.println("Mi Primer Proyecto CON ESTA COSA TAN BONITA");
     }
     
 }
